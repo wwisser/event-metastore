8a1f061b692e9725ec5cd72774cbc9e59f4f4dd0
wwisser <9454122+wwisser@users.noreply.github.com>
2022-08-15 03:00:15 +0200

    Allow CORS for all methods, headers and origins

--- server/src/main/scala/me/wendelin/eventmetastore/EventMetastoreApplication.scala
+++ server/src/main/scala/me/wendelin/eventmetastore/EventMetastoreApplication.scala
@@ -2,13 +2,30 @@ package me.wendelin.eventmetastore
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaRepositories
-class EventMetastoreApplication
+class EventMetastoreApplication {
+
+  @Bean
+  def corsConfigurer(): WebMvcConfigurer = {
+    new WebMvcConfigurer() {
+      override def addCorsMappings(registry: CorsRegistry): Unit = {
+        registry.addMapping("/**")
+          .allowedOrigins("*")
+          .allowedHeaders("*")
+          .allowedMethods("*")
+      }
+    }
+  }
+
+}
 
 object EventMetastoreApplication extends App {
   SpringApplication.run(classOf[EventMetastoreApplication])
