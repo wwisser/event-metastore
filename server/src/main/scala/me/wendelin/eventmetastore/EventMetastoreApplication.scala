fa813eb913541dfc2e6e28103bd89486c084f171
Wendelin Wisser <wisserwendelin@gmail.com>
2022-08-16 21:06:15 +0200

    Make security token configurable

--- server/src/main/scala/me/wendelin/eventmetastore/EventMetastoreApplication.scala
+++ server/src/main/scala/me/wendelin/eventmetastore/EventMetastoreApplication.scala
@@ -6,10 +6,11 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.{Bean}
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.servlet.config.annotation.{CorsRegistry, InterceptorRegistry, WebMvcConfigurer}
+import org.springframework.beans.factory.annotation.Value
 
 @SpringBootApplication
 @EnableScheduling
@@ -31,8 +32,10 @@ class EventMetastoreApplication {
   @Bean
   def interceptorConfigurer(): WebMvcConfigurer = {
     new WebMvcConfigurer() {
+      @Value("${security.token}") private val token: String = null
+
       override def addInterceptors(registry: InterceptorRegistry): Unit = {
-        registry.addInterceptor(new StaticTokenInterceptor).addPathPatterns("/**")
+        registry.addInterceptor(new StaticTokenInterceptor(token)).addPathPatterns("/**")
       }
     }
   }
